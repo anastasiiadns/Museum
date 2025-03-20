312e07b9ff46c41258b8b01b7407a7ce51804405
Anastasiia Denysenko <nastyadenisenko17@gmail.com>
2025-03-20 14:42:41 +0200

    make build

---
+++ Museum.1c9dc3b6.js
@@ -0,0 +1,2 @@
+let swiper=new Swiper(".swiper",{pagination:{el:".swiper-pagination",clickable:!0},grabCursor:!0,breakpoints:{0:{slidesPerView:1.07,spaceBetween:16},498:{slidesPerView:2.1,spaceBetween:20}}});
+//# sourceMappingURL=Museum.1c9dc3b6.js.map
